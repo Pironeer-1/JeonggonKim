502d2e801d4f4217afe6095450844d4beddf1c3d
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-04 21:40:05 +0900

    ch02-2_배열과 JSX 구문

--- ReactNative/ch02/ch02_2/App.tsx
+++ ReactNative/ch02/ch02_2/App.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 import {SafeAreaView, Text} from 'react-native'
 
 export default function App() {
-  const isLoading = false
-  const children = isLoading ? (
-    <Text>Loading...</Text>
-  ) : (
-    <Text>Hello JSX world!</Text>
-  )
+  const children = [
+    <Text>Hello world!</Text>,
+    <Text>Hello world!</Text>,
+    <Text>Hello world!</Text>
+  ]
   return <SafeAreaView>{children}</SafeAreaView>
 }
