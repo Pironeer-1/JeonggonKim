7a224cf7ac74397bc48f8431824d7495e698a49f
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-04 21:37:03 +0900

    ch02_실행문3

--- ReactNative/ch02/ch02_2/App.tsx
+++ ReactNative/ch02/ch02_2/App.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import {SafeAreaView, Text} from 'react-native'
 
 export default function App() {
-  const isLoading = true
-  return (
-    <SafeAreaView>
-      {isLoading && <Text>Loading3...</Text>}
-      {!isLoading && <Text>Hello JSX world!</Text>}
-    </SafeAreaView>
+  const isLoading = false
+  const children = isLoading ? (
+    <Text>Loading...</Text>
+  ) : (
+    <Text>Hello JSX world!</Text>
   )
+  return <SafeAreaView>{children}</SafeAreaView>
 }
