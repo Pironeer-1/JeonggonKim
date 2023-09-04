7415dbc316c286feaa6bb2c1b203b2cc9aa44055
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-05 02:32:44 +0900

    ch02_3 scrollview 추가

--- ReactNative/ch02/ch02_3/App.tsx
+++ ReactNative/ch02/ch02_3/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {SafeAreaView, Text} from 'react-native'
+import {SafeAreaView, Text, ScrollView} from 'react-native'
 import ClassComponent from './src/screens/ClassComponent'
 import ArrowComponent from './src/screens/ArrowComponent'
 import Person from './src/screens/Person'
@@ -10,9 +10,11 @@ const person = D.createRandomPerson()
 export default function App() {
   return (
     <SafeAreaView>
-      <ClassComponent />
-      <ArrowComponent />
-      <Person person={person} />
+      <ScrollView>
+        <ClassComponent />
+        <ArrowComponent />
+        <Person person={person} />
+      </ScrollView>
     </SafeAreaView>
   )
 }
