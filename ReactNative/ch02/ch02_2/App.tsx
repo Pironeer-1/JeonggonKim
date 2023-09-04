84baeba1b286003f64031fbfe5221cc37f987afa
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-04 21:35:16 +0900

    ch02_실행문1

---
+++ ReactNative/ch02/ch02_2/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {SafeAreaView, Text} from 'react-native'
+
+export default function App() {
+  const isLoading = true
+  if (isLoading) {
+    return (
+      <SafeAreaView>
+        <Text>Loading...</Text>
+      </SafeAreaView>
+    )
+  }
+}
