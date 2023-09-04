97aa2ae0f2df302867aec2117b79631d098534aa
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-04 21:45:40 +0900

    Array.map 사용

--- ReactNative/ch02/ch02_2/App.tsx
+++ ReactNative/ch02/ch02_2/App.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import {SafeAreaView, Text} from 'react-native'
 
 export default function App() {
-  const children = [
-    <Text>Hello world!</Text>,
-    <Text>Hello world!</Text>,
-    <Text>Hello world!</Text>
-  ]
+  const children = [1,2,3].map((i) => <Text>Hello World! {i}</Text>)
   return <SafeAreaView>{children}</SafeAreaView>
 }
