c6886ed281209e91b98ef09b57c74e5fbacf7500
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-05 02:21:13 +0900

    ch02_3 타입스크립트로 함수 컴포넌트 구현하기

--- ReactNative/ch02/ch02_3/App.tsx
+++ ReactNative/ch02/ch02_3/App.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import {SafeAreaView, Text} from 'react-native'
 import ClassComponent from './src/screens/ClassComponent'
 import ArrowComponent from './src/screens/ArrowComponent'
+import Person from './src/screens/person'
+import * as D from './src/data'
+
+const Person = D.createRandomPerson()
 
 export default function App() {
   return (
     <SafeAreaView>
       <ClassComponent />
       <ArrowComponent />
+      <Person person={person} />
     </SafeAreaView>
   )
 }
