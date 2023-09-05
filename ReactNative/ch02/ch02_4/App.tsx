39257833776ae723ab9c30f487880a718e7f386a
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-05 13:56:42 +0900

    ch02_4 textinput

--- ReactNative/ch02/ch02_4/App.tsx
+++ ReactNative/ch02/ch02_4/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SafeAreaView, Alert, Button } from 'react-native'
 import {TouchableOpacity, TouchableHighlight, Text} from 'react-native'
+import {TextInput} from 'react-native'
 
 const onPress= () => Alert.alert('home pressed','message')
 
@@ -14,6 +15,13 @@ export default function App() {
     <TouchableHighlight onPress={onPress}>
       <Text>TouchableHighlight</Text>
     </TouchableHighlight>
+    <TextInput
+      placeholder="enter your name"
+      onChangeText={(text: string) => console.log(text)}
+      onFocus={() => console.log('onFocus')}
+      onBlur={() => console.log('onBlur')}
+      onEndEditing={()=> console.log('onEndEditing')}
+      />
   </SafeAreaView>
   )
 }
