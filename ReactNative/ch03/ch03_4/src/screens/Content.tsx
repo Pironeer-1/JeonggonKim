84fff7dfc9e47af7292957e32a960325b4b60350
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-05 17:17:13 +0900

    ch03_4 flex, overflow

--- ReactNative/ch03/ch03_4/src/screens/Content.tsx
+++ ReactNative/ch03/ch03_4/src/screens/Content.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import {StyleSheet, View, Image, ScrollView} from 'react-native'
 import {MD2Colors} from 'react-native-paper'
 import * as D from '../data'
 
+const avatars = D.makeArray(200).map((notUsed)=> D.randomAvatarUrl())
+
 const title = 'Content'
-export default function CopyMe() {
-    return (
-        <View style={[styles.view]}>
-            <Text style={[styles.text]}>{title}</Text>
-            <View style={[{flex:0, backgroundColor: MD2Colors.red500}]}>
-                <Text>flex: 0</Text>
-            </View>
-            <View style={[{flex:100, backgroundColor: MD2Colors.green500}]}>
-                <Text>flex: 1</Text>
-            </View>
-            <View style={[{flex:200, backgroundColor: MD2Colors.purple500}]}>
-                <Text>flex: 2</Text>
-            </View>
+export default function Content() {
+    const children = avatars.map((avatarUrl, index) => (
+        <View key={index.toString()} style={styles.avatarView}>
+            <Image style={styles.avatar} source={{uri: avatarUrl}} />
         </View>
+    ))
+    return (
+        <ScrollView contentContainerStyle={[styles.view]}>{children}</ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    view: {flex: 1, padding: 5, backgroundColor: MD2Colors.lightBlue500},
-    text: {fontSize: 20, color: 'white'}
+    view: {
+        flexWrap: 'wrap', 
+        flexDirection: 'row',
+        justifyContent: 'center',
+        padding: 5
+    },
+    text: {fontSize: 20},
+    avatarView: {padding: 3},
+    avatar: {width: 50, height: 50, borderRadius: 25},
 })
