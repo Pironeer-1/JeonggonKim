c0b5af774998871f3a1fc4a21805169d2263b207
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-08 14:49:03 +0900

    node.js ch01_25

---
+++ node.js/web2-nodejs/syntax/function2.js
@@ -0,0 +1,10 @@
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+
+function sum(first,second) {
+    console.log(first)
+    return (first + second);
+    console.log(second)
+}
+
+console.log(sum(2,4))
