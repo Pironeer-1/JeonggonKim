32869fb14196d79fc6b3983f93a07ad76f5b75c1
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-11 19:22:09 +0900

    [add] Header 완료

---
+++ ReactNative/Instagram-Clone/components/home/Header.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
+
+const Header = () => {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <Image style = {styles.logo} 
+                source={require('../../assets/header-logo.png')} />
+            </TouchableOpacity>
+            <View style={styles.iconsContainer}>
+                <TouchableOpacity>
+                    <Image style = {styles.icon} 
+                    source={require('../../assets/header-plus.png')} />
+                </TouchableOpacity> 
+                <TouchableOpacity>
+                    <Image style = {styles.icon} 
+                    source={require('../../assets/header-heart.png')} />
+                </TouchableOpacity>                
+                <TouchableOpacity>
+                    <View style={styles.unreadBadge}>
+                        <Text style={styles.unreadBadgeText}>11</Text>
+                    </View>
+                    <Image style = {styles.icon} 
+                    source={require('../../assets/header-chat.png')} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles= StyleSheet.create({
+    container : {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        marginTop: 20,
+    },
+    iconsContainer : {
+        flexDirection: 'row',
+    },
+    logo : {
+        width: 100,
+        height:50,
+        resizeMode: 'contain',
+    },
+    icon : {
+        width: 30,
+        height: 30,
+        marginLeft: 10,
+        resizeMode: 'contain',
+    },
+    unreadBadge: {
+        backgroundColor: '#FF3250',
+        position: 'absolute',
+        left: 20,
+        bottom: 18,
+        width: 25,
+        height: 18,
+        borderRadius:25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 100,
+    },
+
+    unreadBadgeText: {
+        color: 'white',
+        fontWeight: '600',
+    }
+})
+
+export default Header
+
