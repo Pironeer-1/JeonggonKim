a200de62a96494c543ca071defe8bf1a527a99c9
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-12 13:56:38 +0900

    expo가 너무 안돼서 android cli로 바꿈

---
+++ ReactNative/instagram_clone2/components/home/Post.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {View, Text, StyleSheet,Image} from 'react-native'
+import {Divider} from 'react-native-elements'
+
+const Post = ({ post}) => {
+    return (
+        <View style={{marginBottom:30}}>
+            <Divider width={1} orientation='vertical' />
+            <PostHeader post={post} />
+        </View>
+    )
+}
+
+const PostHeader = ({post}) => (
+    <View style ={{flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    margin:5, 
+    alignItems:'center'}}>
+        <View style={{flexDirection:'row', alignItems:'center'}}>
+            <Image source={{uri:post.profile_picture}} style={styles.story}/>   
+            <Text style={{color:'white', marginLeft:5}}> 
+            {post.user}
+            </Text>
+        </View>
+        <Text style={{color:'white'}}> ... </Text>
+    </View>
+)
+export default Post
+
+const styles = StyleSheet.create({
+    story : {
+        width: 35,
+        height:35,
+        borderRadius: 50,
+        marginLeft: 6,
+        borderWidth: 1.6,
+        borderColor: '#ff8501',
+    },
+})
---
+++ ReactNative/instagram_clone2/data/users.js
@@ -0,0 +1,47 @@
+export const USERS = [
+    {
+        user : 'clverqazi',
+        image:'https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow-770x436.jpg'
+
+    },
+    {
+        user : 'clverqaziadsfasdf',
+        image:'https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow-770x436.jpg'
+
+    },
+    {
+        user : 'clverqazi',
+        image:'https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow-770x436.jpg'
+
+    },
+    {
+        user : 'clverqazi',
+        image:'https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow-770x436.jpg'
+
+    },
+    {
+        user : 'clverqazi',
+        image:'https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow-770x436.jpg'
+
+    },
+    {
+        user : 'clverqazi',
+        image:'https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow-770x436.jpg'
+
+    },
+    {
+        user : 'clverqazi',
+        image:'https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow-770x436.jpg'
+
+    },
+    {
+        user : 'clverqazi',
+        image:'https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow-770x436.jpg'
+
+    },
+    {
+        user : 'clverqazi',
+        image:'https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow-770x436.jpg'
+
+    },
+]
