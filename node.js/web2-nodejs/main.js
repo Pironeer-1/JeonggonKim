db846fcb6e4c14d13ef48dec6fa2b90d79890ee7
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-16 03:12:23 +0900

    node.js ch01_46

--- node.js/web2-nodejs/main.js
+++ node.js/web2-nodejs/main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url')
-var qs = require('querystring')
-var template = require('./lib/template.js')
+var url = require('url');
+var qs = require('querystring');
+var template = require('./lib/template.js');
+var path = require('path');
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
@@ -21,8 +22,9 @@ var app = http.createServer(function (request, response) {
             });
         } else {
             fs.readdir('./data', function (error, filelist) {
+                var filteredId = path.parse(queryData.id).base
                 var list = template.list(filelist);
-                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
                     var title = queryData.id
                     var html = template.HTML(title, list, `<h2>${title}</h2><p>${description}</p>`,
                     `<a href="/create">create</a> 
@@ -72,7 +74,8 @@ var app = http.createServer(function (request, response) {
         });
     } else if (pathname ==='/update') {
         fs.readdir('./data', function (error, filelist) {
-            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+            var filteredId = path.parse(queryData.id).base
+            fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
                 var title = queryData.id;
                 var list = template.list(filelist);
                 var html = template.HTML(title, list, 
@@ -123,7 +126,8 @@ var app = http.createServer(function (request, response) {
         request.on('end', function() {
             var post = qs.parse(body);
             var id = post.id;
-            fs.unlink(`data/${id}`,function(error) {
+            var filteredId = path.parse(id).base
+            fs.unlink(`data/${filteredId}`,function(error) {
                 response.writeHead(302, {Location: `/`});
                 response.end();
             })
