d6b210216436366484f96494682192f22edf2a10
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-16 03:26:19 +0900

    node.js ch01_48

--- node.js/web2-nodejs/main.js
+++ node.js/web2-nodejs/main.js
@@ -4,6 +4,7 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
+var sanitizeHtml = require('saniztize-html')
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
@@ -26,12 +27,16 @@ var app = http.createServer(function (request, response) {
                 var list = template.list(filelist);
                 fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
                     var title = queryData.id
-                    var html = template.HTML(title, list, `<h2>${title}</h2><p>${description}</p>`,
+                    var sanitizedTitle = sanitizeHtml (title);
+                    var sanitizedDescription = sanitizeHtml(description, {
+                        allowedTags:['h1']
+                    })
+                    var html = template.HTML(sanitizedTitle, list, `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
                     `<a href="/create">create</a> 
-                    <a href="/update?id=${title}">update</a>
+                    <a href="/update?id=${sanitizedTitle}">update</a>
                     <form action="delete_process" method="post"
                     onsubmit="return confirm('진짜로 삭제?');">
-                        <input type="hidden" name="id" value="${title}"/>
+                        <input type="hidden" name="id" value="${sanitizedTitle}"/>
                         <input type="submit" value="delete"/>
                     </form>
                     `);
