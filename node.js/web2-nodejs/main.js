112d7621d960abb700d713e13e29c5af6bff1142
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-16 01:51:40 +0900

    node.js ch01_35

--- node.js/web2-nodejs/main.js
+++ node.js/web2-nodejs/main.js
@@ -52,7 +52,7 @@ var app = http.createServer(function (request, response) {
                 var list = templateList(filelist);
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id
-                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`,`<a href="/create">create</a> <a href="/update">update</a>`);
+                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
                     response.writeHead(200)
                     response.end(template);
                 });
@@ -63,7 +63,7 @@ var app = http.createServer(function (request, response) {
             var title = 'WEB - create';
             var list = templateList(filelist);
             var template = templateHTML(title, list, `
-            <form action="http://localhost:3000/create_process" method="post">
+            <form action="/create_process" method="post">
                 <p><input type="text" name="title" placeholder="title"></p>
                 <p>
                     <textarea name="description" placeholder="description"></textarea>
@@ -71,7 +71,7 @@ var app = http.createServer(function (request, response) {
                 <p>
                     <input type="submit">
                 </p>
-            </form>`
+            </form>`,''
             );
             response.writeHead(200)
             response.end(template);
@@ -90,8 +90,29 @@ var app = http.createServer(function (request, response) {
                 response.end()
             })
         });
-    } 
-    else {
+    } else if (pathname ==='/update') {
+        fs.readdir('./data', function (error, filelist) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                var title = queryData.id;
+                var list = templateList(filelist);
+                var template = templateHTML(title, list, 
+                `
+                <form action="/update_process" method="post">
+                    <input type="hidden" name="id" value="${title}"
+                    <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                    <p>
+                        <textarea name="description" placeholder="description">${description}</textarea>
+                    </p>
+                    <p>
+                        <input type="submit">
+                    </p>
+                </form>`,
+                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                response.writeHead(200)
+                response.end(template);
+            });
+        })
+    } else {
         response.writeHead(404);
         response.end('Not Found');
     }
