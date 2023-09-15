077fe9118933ce725ae7ab01e6d62f682b250bc8
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-16 02:19:44 +0900

    node.js ch01_37

--- node.js/web2-nodejs/main.js
+++ node.js/web2-nodejs/main.js
@@ -52,7 +52,15 @@ var app = http.createServer(function (request, response) {
                 var list = templateList(filelist);
                 fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
                     var title = queryData.id
-                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`,
+                    `<a href="/create">create</a> 
+                    <a href="/update?id=${title}">update</a>
+                    <form action="delete_process" method="post"
+                    onsubmit="return confirm('진짜로 삭제?');">
+                        <input type="hidden" name="id" value="${title}"/>
+                        <input type="submit" value="delete"/>
+                    </form>
+                    `);
                     response.writeHead(200)
                     response.end(template);
                 });
@@ -107,7 +115,10 @@ var app = http.createServer(function (request, response) {
                         <input type="submit">
                     </p>
                 </form>`,
-                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                `<a href="/create">create</a> 
+                <a href="/update?id=${title}">update</a>
+                <a href="/delete?id=${title}">delete</a>
+                `);
                 response.writeHead(200)
                 response.end(template);
             });
