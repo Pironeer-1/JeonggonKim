0741739456bc2474cc7338f958533debed49f7c0
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-21 18:59:35 +0900

    node.js ch02_6

--- node.js/ch.2/main.js
+++ node.js/ch.2/main.js
@@ -61,10 +61,11 @@ var app = http.createServer(function(request, response) {
             });
         }
     } else if(pathname === '/create') {
-        fs.readdir('./data', function(error, filelist) {
-            var title = 'WEB - create';
-            var list = template.list(filelist);
-            var html = template.HTML(title, list, `
+        db.query(`SELECT * FROM topic`, function(error, topics){
+            var title = 'Craete'
+            var list = template.list(topics);
+            var html= template.HTML(title, list, 
+                `
                 <form action="/create_process" method="post">
                     <p><input type="text" name="title" placeholder="title"></p>
                     <p>
@@ -73,8 +74,9 @@ var app = http.createServer(function(request, response) {
                     <p>
                         <input type="submit">
                     </p>
-                </form>
-            `, '');
+                </form>`, 
+                `<a href="/create">create</a>`
+            );
             response.writeHead(200);
             response.end(html);
         });
@@ -85,12 +87,18 @@ var app = http.createServer(function(request, response) {
         });
         request.on('end', function() {
             var post = qs.parse(body);
-            var title = post.title;
-            var description = post.description;
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-                response.writeHead(302, {Location: `/?id=${title}`});
-                response.end();
-            });
+            db.query(`
+                INSERT INTO topic (title, description, created, author_id)
+                VALUES(?,?,NOW(),?)`,
+                [post.title, post.description, 1],
+                function(error, result) {
+                    if(error) {
+                        throw error;
+                    }
+                    response.writeHead(302, {Location: `/?id=${result.insertId}`});
+                    response.end();
+                }
+            );
         });
     } else if(pathname === '/update') {
         fs.readdir('./data', function(error, filelist) {
