835f036418ac4d9b3554eee3873c30e2bb2967d2
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-22 01:40:16 +0900

    node.js ch02_8

--- node.js/ch.2/main.js
+++ node.js/ch.2/main.js
@@ -149,10 +149,11 @@ var app = http.createServer(function(request, response) {
         });
         request.on('end', function() {
             var post = qs.parse(body);
-            var id = post.id;
-            var filteredId = path.parse(id).base;
-            fs.unlink(`data/${filteredId}`, function(error) {
-                response.writeHead(302, {Location: `/?id=${post.id}`});
+            db.query('DELETE FROM topic WHERE id= ?', [post.id], function(error, result){
+                if (error) {
+                    throw error;
+                }
+                response.writeHead(302, {Location: `/`});
                 response.end();
             });
         });
