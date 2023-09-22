bed68b273b8618282277378b9f4dff2acb9c2c4b
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-22 22:37:09 +0900

    node.js ch03_5

--- node.js/ch.3 Express/main.js
+++ node.js/ch.3 Express/main.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+
 
 app.get('/', function(request,response){
     fs.readdir('./data', function(error, filelist) {
@@ -16,11 +19,31 @@ app.get('/', function(request,response){
     });
 
 })
-app.get('/page', function(req,res){
-    return res.send('/page')
+app.get('/page/:pageId', function(request,response){
+    fs.readdir('./data', function(error, filelist) {
+        var filteredId = path.parse(request.params.pageId).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+            var title = request.params.pageId;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+                allowedTags:['h1']
+            });
+            var list = template.list(filelist);
+            var html = template.HTML(sanitizedTitle, list,
+                `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`,
+                `<a href="/create">create</a>
+                <a href="/update?id=${sanitizedTitle}">update</a>
+                <form action="delete_process" method="post">
+                    <input type="hidden" name="id" value="${sanitizedTitle}">
+                    <input type="submit" value="delete">
+                </form>`
+            );
+            response.send(html)
+        });
+    });
 });
 app.listen(3000,function(){
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!');
 });
 
 
